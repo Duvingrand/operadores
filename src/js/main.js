706935152b50b71f2b55c1bd020fb39e5d2faf2d
Duvingrand <164047332+Duvingrand@users.noreply.github.com>
2024-04-18 13:44:40 -0500

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -5,78 +5,97 @@ console.groupEnd();
 
 // Exercise 2: ¿Es 45 menor que 30?
 console.groupCollapsed("Exercise 2: ¿Es 45 menor que 30?");
-    console.log
+    console.log(45<30)
 console.groupEnd();
 
 // Exercise 3: ¿Son 50 y "50" iguales?
 console.groupCollapsed("Exercise 3: ¿Son 50 y '50' iguales?");
+    console.log(50=="50")
 console.groupEnd();
 
 // Exercise 4: ¿Es 40 estrictamente igual a 40 convertido a número?
 console.groupCollapsed("Exercise 4: ¿Es 40 estrictamente igual a 40 convertido a número?");
+    console.log(40===Number("40"))
 console.groupEnd();
 
 // Exercise 5: ¿Son "apple" y "orange" diferentes?
 console.groupCollapsed("Exercise 5: ¿Son 'apple' y 'orange' diferentes?");
+    console.log("apple"=="oranges")
 console.groupEnd();
 
 // Exercise 6: ¿Es "apple" estrictamente diferente de "Apple"?
 console.groupCollapsed("Exercise 6: ¿Es 'apple' estrictamente diferente de 'Apple'?");
+    console.log("apple"=="Apple")
 console.groupEnd();
 
 // Exercise 7: ¿Cuál es el valor de una variable sin definir?
 console.groupCollapsed("Exercise 7: ¿Cuál es el valor de una variable sin definir?");
+    let variableNo
+    console.log(variableNo)
 console.groupEnd();
 
 // Exercise 8: ¿Cuál es el tipo de dato de una variable sin definir?
 console.groupCollapsed("Exercise 8: ¿Cuál es el tipo de dato de una variable sin definir?");
+    console.log(typeof(variableNo))
 console.groupEnd();
 
 // Exercise 9: ¿Es null igual a undefined?
 console.groupCollapsed("Exercise 9: ¿Es null igual a undefined?");
+    console.log(variableNo==null)
 console.groupEnd();
 
 // Exercise 10: ¿Son null y undefined estrictamente iguales?
 console.groupCollapsed("Exercise 10: ¿Son null y undefined estrictamente iguales?");
+    console.log(null===undefined)
 console.groupEnd();
 
 // Exercise 11: ¿Es "2.75" mayor que 2.5?
 console.groupCollapsed("Exercise 11: ¿Es '2.75' mayor que 2.5?");
+    console.log(2.75>2.5)
 console.groupEnd();
 
 console.groupCollapsed("Exercise 12: ¿Es '2.75' mayor que 2.75?");
+    console.log(2.75<2.75)
 console.groupEnd();
 
 // Exercise 13: ¿Es "2.75" mayor o igual a 2.75?
 console.groupCollapsed("Exercise 13: ¿Es '2.75' mayor o igual a 2.75?");
+    console.log("2.75">=2.75)
 console.groupEnd();
 
 // Exercise 14: ¿Es "2.75" menor o igual a 2.75?
 console.groupCollapsed("Exercise 14: ¿Es '2.75' menor o igual a 2.75?");
+    console.log("2.75"<=2.75)
 console.groupEnd();
 
 // Exercise 15: ¿Es "2.75" un número mayor a 2.8?
 console.groupCollapsed("Exercise 15: ¿Es '2.75' un número mayor a 2.8?");
+    console.log("2.75">2.8)
 console.groupEnd();
 
 // Exercise 16: ¿Es "2.75" un número menor a 2.7?
 console.groupCollapsed("Exercise 16: ¿Es '2.75' un número menor a 2.7?");
+    console.log("2.75"<2.7)
 console.groupEnd();
 
 // Exercise 17: ¿Es el nombre "John" diferente de "john"?
 console.groupCollapsed("Exercise 17: ¿Es el nombre 'John' diferente de 'john'?");
+    console.log("Jonh"!="jonh")
 console.groupEnd();
 
 // Exercise 18: ¿Es el nombre "John" diferente de "Jon"?
 console.groupCollapsed("Exercise 18: ¿Es el nombre 'John' diferente de 'Jon'?");
+    console.log("John"!="Jon")
 console.groupEnd();
 
 // Exercise 19: ¿Es la edad 30 estrictamente diferente de "30"?
 console.groupCollapsed("Exercise 19: ¿Es la edad 30 estrictamente diferente de '30'?");
+    console.log(30==="30")
 console.groupEnd();
 
 // Exercise 20: ¿Cuál es el resultado de desplazar a la derecha 25 dos veces en bits?
 console.groupCollapsed("Exercise 20: ¿Cuál es el resultado de desplazar a la derecha 25 dos veces en bits?");
+    console.log()
 console.groupEnd();
 
 // Exercise 21: ¿Cuál es el resultado de desplazar a la izquierda 8 tres veces en bits?
@@ -85,36 +104,46 @@ console.groupEnd();
 
 // Exercise 22: ¿Son 15 y 15 iguales?
 console.groupCollapsed("Exercise 22: ¿Son 15 y 15 iguales?");
+    console.log(15==15)
 console.groupEnd();
 
 // Exercise 23: ¿Son 20 y 30 diferentes?
 console.groupCollapsed("Exercise 23: ¿Son 20 y 30 diferentes?");
+    console.log(20!=30)
 console.groupEnd();
 
 // Exercise 24: ¿Es "15" mayor que 30?
 console.groupCollapsed("Exercise 24: ¿Es '15' mayor que 30?");
+    console.log("15">30)
 console.groupEnd();
 
 // Exercise 25: ¿Es 20 menor que 40?
 console.groupCollapsed("Exercise 25: ¿Es 20 menor que 40?");
+    console.log(20<40)
 console.groupEnd();
 
 // Exercise 26: ¿Es 100 mayor o igual a "99"?
 console.groupCollapsed("Exercise 26: ¿Es 100 mayor o igual a '99'?");
+    console.log(100>="99")
 console.groupEnd();
 
 // Exercise 27: ¿Es 500 estrictamente igual a "500"?
 console.groupCollapsed("Exercise 27: ¿Es 500 estrictamente igual a '500'?");
+    console.log(500==="500")
 console.groupEnd();
 
 // Exercise 28: ¿Es 750 estrictamente igual a 750?
 console.groupCollapsed("Exercise 28: ¿Es 750 estrictamente igual a 750?");
+    console.log(750===750)
 console.groupEnd();
 
 // Exercise 29: ¿Es 5 estrictamente diferente de "5"?
 console.groupCollapsed("Exercise 29: ¿Es 5 estrictamente diferente de '5'?");
+    console.log(5==="5")
 console.groupEnd();
 
 // Exercise 30: ¿Es 3 mayor que 5 y 4 menor que 6? (Operadores lógicos)
 console.groupCollapsed("Exercise 30: ¿Es 3 mayor que 5 y 4 menor que 6? (Operadores lógicos)");
+    console.log(3>5 && 4 <6)
 console.groupEnd();
+
